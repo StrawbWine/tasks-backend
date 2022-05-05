38b518538088744e7692464be366025d364dbc8e
Severin Hanevik <severin.hanevik@gmail.com>
2022-05-05 21:07:48 +0200

    ✨Add HTTP POST for users

--- src/main/java/com/strawbwine/tasks/backend/UserController.java
+++ src/main/java/com/strawbwine/tasks/backend/UserController.java
@@ -1,15 +1,20 @@
 package com.strawbwine.tasks.backend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
   private final IDatabase cosmos = new CosmosDB();
+
   @GetMapping(value = "/user", produces = "application/json")
-  public UserDAO user(@RequestParam(value = "name") String name) {
+  public UserDAO getUser(@RequestParam(value = "name") String name) {
     return new UserDAO(cosmos.fetchUser(name));
+  }
+
+  @PostMapping(value = "/user", consumes = "application/json", produces = "application/json")
+  public UserDAO postUser(@RequestBody UserDAO userDAO) {
+    cosmos.write(new User(userDAO));
+    return userDAO;
   }
 }
