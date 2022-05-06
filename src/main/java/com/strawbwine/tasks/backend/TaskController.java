0a0505626676d940e3e5e4939ab0b7b1d2e33654
Severin Hanevik <severin.hanevik@gmail.com>
2022-05-06 20:26:05 +0200

    ✨Add basic POST endpoint for tasks

--- src/main/java/com/strawbwine/tasks/backend/TaskController.java
+++ src/main/java/com/strawbwine/tasks/backend/TaskController.java
@@ -1,15 +1,20 @@
 package com.strawbwine.tasks.backend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskController {
 
   private final IDatabase cosmos = new CosmosDB();
+
   @GetMapping(value = "/task", produces = "application/json")
-  public TodoItemDAO user(@RequestParam(value = "name") String name) {
+  public TodoItemDAO getUser(@RequestParam(value = "name") String name) {
     return new TodoItemDAO(cosmos.fetchTask(name));
+  }
+
+  @PostMapping(value = "/task", consumes = "application/json", produces = "application/json")
+  public TodoItemDAO postUser(@RequestBody TodoItemDAO taskDAO) {
+    cosmos.write(new TodoItem(taskDAO));
+    return taskDAO;
   }
 }
